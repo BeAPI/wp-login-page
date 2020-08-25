b4f47476161f68ac2db6a31a80418a9a695c2fa0
Nicolas Juen <njuen@beapi.fr>
2020-08-25 19:12:49 +0200

    Add basic code for the style enqueueing

--- wp-login-page.php
+++ wp-login-page.php
@@ -17,3 +17,29 @@ namespace BEAPI\WP_Login_Page;
 
 // Standard plugin security, keep this line in place.
 defined( 'ABSPATH' ) || die();
+
+
+add_action( 'login_head', __NAMESPACE__ . '\\enqueue_asset' );
+function enqueue_asset() {
+	wp_enqueue_style( 'wp-login-page-css' );
+}
+
+add_action( 'wp', __NAMESPACE__ . '\\register_asset' );
+function register_asset() {
+	wp_register_style( 'wp-login-page-css', get_stylesheet_uri(), [ 'login' ], '1.0.0' );
+}
+
+function get_stylesheet_uri() {
+	$file = get_theme_file_uri( '/dist/assets/login.css' );
+	if ( ! empty( $file ) ) {
+		return $file;
+	}
+
+	$platform_filename = apply_filters( 'wp-login-page-platform-css', 'wp-login-page/login.css' );
+
+	if ( is_file( WP_CONTENT_DIR . '/' . $platform_filename ) ) {
+		return WP_CONTENT_URL . '/' . $platform_filename;
+	}
+
+	return WPMU_PLUGIN_URL . '/wp-login-page/assets/login.css';
+}
