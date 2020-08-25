b32b929030588cba6dc8b2c0f2d842ba29d4c7a8
Nicolas Juen <njuen@beapi.fr>
2020-08-25 18:44:30 +0200

    Initilize plugin

---
+++ wp-login-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       WP Login page
+ * Plugin URI:        https://github.com/BeAPI/wp-login-page
+ * Description:       Customize the login page with CSS/images
+ * Version:           1.0.0
+ * Requires PHP:      5.6
+ * Author:            Be API
+ * Author URI:        https://beapi.fr
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-login-page
+ * Domain Path:
+ */
+
+namespace BEAPI\WP_Login_Page;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) || die();
